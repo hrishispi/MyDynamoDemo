9d278e10a9b6d344bd1250212d9a164c65c0c83a
Karambelkar <hrishispi@gmail.com>
2016-10-01 22:16:45 -0400

    externalized properties

--- src/main/java/com/dynamodemo/config/ProdDynamoConfig.java
+++ src/main/java/com/dynamodemo/config/ProdDynamoConfig.java
@@ -6,41 +6,30 @@ import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRep
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Profile;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
-
+@Profile("prod")
 @Configuration
-@PropertySource(value="file:///Users/uys445/Documents/workspace/prop/application-dev.properties")
 @EnableDynamoDBRepositories(basePackages = "com.dynamodemo")
-public class DynamoConfig {
+public class ProdDynamoConfig {
 
     @Value("${amazon.dynamodb.endpoint}")
     private String amazonDynamoDBEndpoint;
 
-    @Value("${amazon.aws.accesskey}")
-    private String amazonAWSAccessKey;
-
-    @Value("${amazon.aws.secretkey}")
-    private String amazonAWSSecretKey;
-
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         AmazonDynamoDB amazonDynamoDB = new AmazonDynamoDBClient(
-                amazonAWSCredentials());
+                new InstanceProfileCredentialsProvider());
         if (StringUtils.isNotEmpty(amazonDynamoDBEndpoint)) {
             amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
         }
         return amazonDynamoDB;
-    }
-
-    @Bean
-    public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
     }
 
 }
